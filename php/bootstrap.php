4dbae3491b2083ac5c9cec6acf5c9a8cf40e3c2b
pinksalmon <zachwilliamson91@gmail.com>
2015-09-19 21:47:36 -0500

    updated bootstrap to create databases for first time user

--- php/bootstrap.php
+++ php/bootstrap.php
@@ -1,6 +1,27 @@
 <?php
-    $hostname = '';
-    $dbname = '';
-    $username = '';
-    $password = '';
+    $hostname = 'null';
+    $dbname = 'null';
+    $username = 'null';
+    $password = 'null';
+    
+    if($dbname) {
+        $conn = new mysqli($hostname, $username, $password);
+        if($conn->query("CREATE DATABASE $dbname") === true) {
+            $conn = new mysqli($hostname, $username, $password, $dbname);
+            $sql = "CREATE TABLE food (
+                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(30),
+                unit VARCHAR(30),
+                quantity int(6)
+            )";
+            $conn->query($sql);
+            $sql = "CREATE TABLE recipes (
+                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(30),
+                instructions TEXT,
+                food TEXT 
+            )";
+            $conn->query($sql);
+        } 
+    }
 ?>
